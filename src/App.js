fa9bd94aee903cea4a6995404cd3544628bb531e
Ido Karelitz <idoinfo@gmail.com>
2022-09-17 11:45:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav/MainNav';
+import React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { Container } from '@mui/system';
+import Trending from './Pages/Trending/Trending';
+import Search from './Pages/Search/Search';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+    <Header />
+    <SimpleBottomNavigation />
+    <div className='app'>
+      <Container>
+      <Routes>
+      <Route path="/" element={<Trending />}/>
+      <Route path="/movies" element={<Movies />}/>
+      <Route path="/series" element={<Series />}/>
+      <Route path="/search" element={<Search />}/>
+      </Routes>
+      </Container>
+    </div>
+    
+    
+    
+    </BrowserRouter>
+      
+    
+  );
+}
+
+export default App;
---
+++ src/components/Pagination/CustomPagination.js
@@ -0,0 +1,38 @@
+import { Pagination } from '@mui/material'
+import React from 'react'
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/system'
+
+const darkTheme=createTheme({
+    palette: {
+        mode: 'dark',
+      },
+})
+
+const CustomPagination = ({setPage, numOfPages = 10}) => {
+    const handlePageChange = (page) => {
+        setPage(page);
+        window.scroll(0,0);
+    };
+
+  return (
+    <div
+    style={{
+        width: '100%',
+        display:'flex',
+        justifyContent:'center',
+        marginTop:10,
+
+    }}>
+        <ThemeProvider theme={darkTheme}>
+            <Pagination 
+            count={numOfPages} variant="outlined"
+            onChange={(e) => handlePageChange(e.target.textContent)}
+            color = 'primary'/>
+        </ThemeProvider>
+        
+    </div>
+  )
+}
+
+export default CustomPagination
